0a3f4ea9500f5484ba5d928b3fb93d14b9494e0d
oliverjunk <oliver@meta-app.eu>
2018-03-20 18:21:12 +0100

    fixed timeTravel

--- test/timeTravel.js
+++ test/timeTravel.js
@@ -1,26 +1,13 @@
 module.exports = seconds =>
   new Promise((resolve, reject) => {
-      web3.currentProvider.sendAsync(
-          {
-              jsonrpc: "2.0",
-              method: "evm_increaseTime",
-              params: [ seconds ],
-              id: new Date().getTime(),
-          },
-      (err1) => {
-          if (err1) return reject(err1);
-          web3.currentProvider.sendAsync(
-              {
-                  jsonrpc: "2.0",
-                  method: "evm_mine",
-                  params: [],
-                  id: new Date().getTime(),
-              },
-          (err2) => {
-              if (err2) return reject(err2);
-              resolve();
-          },
-        );
-      },
-    );
-  });
+        web3.currentProvider.sendAsync({
+          jsonrpc: "2.0",
+          method: "evm_increaseTime",
+          params: [seconds], // 86400 is num seconds in day
+          id: new Date().getTime()
+        }, (err, result) => {
+          if(err){ return reject(err) }
+          return resolve(result)
+        });
+      });
+   
